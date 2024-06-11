5d107e488500fdece95755bdc7e7868b6fd739c3
JoseOnGit <dodo.babjak@gmail.com>
2024-06-11 22:05:23 +0200

    Fix 'Add request' for stores

--- src/slices/storeRequest.ts
+++ src/slices/storeRequest.ts
@@ -48,7 +48,7 @@ const initialState: RequestsState = {
   pickedDaysDetail: [],
 };
 
-export const addRequest = createAsyncThunk(
+export const addStoreRequest = createAsyncThunk(
   "storeRequest/addReqest",
   async (request: RequestType) => {
     try {
@@ -84,7 +84,7 @@ export const getStoreRequests = createAsyncThunk(
   }
 );
 
-export const removeRequest = createAsyncThunk(
+export const removeStoreRequest = createAsyncThunk(
   "storeRequest/removeRequest",
   async (request: RequestType) => {
     try {
@@ -112,10 +112,10 @@ export const storeRequestSlice = createSlice({
   name: "storeRequest",
   initialState,
   reducers: {
-    addPickedDay(state, action) {
+    addPickedDayByStore(state, action) {
       state.pickedDaysDetail = [...state.pickedDaysDetail, action.payload];
     },
-    changePickedDay(state, action) {
+    changePickedDayByStore(state, action) {
       state.pickedDaysDetail = state.pickedDaysDetail.map((pickedDay) => {
         if (pickedDay.day === action.payload.day) {
           return action.payload;
@@ -123,7 +123,7 @@ export const storeRequestSlice = createSlice({
         return pickedDay;
       });
     },
-    removePickedDay(state, action) {
+    removePickedDayByStore(state, action) {
       state.pickedDaysDetail = state.pickedDaysDetail.filter(
         (pickedDay) => pickedDay.day !== action.payload.day
       );
@@ -133,11 +133,11 @@ export const storeRequestSlice = createSlice({
     builder
 
       // ADD REQUEST
-      .addCase(addRequest.pending, (state) => {
+      .addCase(addStoreRequest.pending, (state) => {
         state.requestsDetail.status = "loading";
         state.requestsDetail.error = null;
       })
-      .addCase(addRequest.fulfilled, (state, action) => {
+      .addCase(addStoreRequest.fulfilled, (state, action) => {
         if (action.payload.response?.data?.message) {
           state.requestsDetail.status = "failed";
           state.requestsDetail.requests = [];
@@ -155,7 +155,7 @@ export const storeRequestSlice = createSlice({
           state.requestsDetail.error = null;
         }
       })
-      .addCase(addRequest.rejected, (state, action) => {
+      .addCase(addStoreRequest.rejected, (state, action) => {
         state.requestsDetail.status = "failed";
         state.requestsDetail.error = action.error.message || null;
       })
@@ -227,12 +227,12 @@ export const storeRequestSlice = createSlice({
       })
 
       // REMOVE REQUEST
-      .addCase(removeRequest.pending, (state, action) => {
+      .addCase(removeStoreRequest.pending, (state, action) => {
         state.requestsDetail.status = "loading";
         state.requestsDetail.loaded = action.meta.arg.id;
         state.requestsDetail.error = null;
       })
-      .addCase(removeRequest.fulfilled, (state, action) => {
+      .addCase(removeStoreRequest.fulfilled, (state, action) => {
         if (action.payload.response?.data?.message) {
           state.requestsDetail.status = "failed";
           state.requestsDetail.error =
@@ -247,7 +247,7 @@ export const storeRequestSlice = createSlice({
           state.requestsDetail.loaded = null;
         }
       })
-      .addCase(removeRequest.rejected, (state, action) => {
+      .addCase(removeStoreRequest.rejected, (state, action) => {
         state.requestsDetail.status = "failed";
         state.requestsDetail.error = action.error.message || null;
         state.requestsDetail.loaded = null;
@@ -284,8 +284,11 @@ export const storeRequestSlice = createSlice({
       }),
 });
 
-export const { addPickedDay, changePickedDay, removePickedDay } =
-  storeRequestSlice.actions;
+export const {
+  addPickedDayByStore,
+  changePickedDayByStore,
+  removePickedDayByStore,
+} = storeRequestSlice.actions;
 
 export const storeRequestsSelector = (state: RootState): RequestType[] =>
   state.storeRequest.requestsDetail.requests;
